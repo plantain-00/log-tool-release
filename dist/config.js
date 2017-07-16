619ed564390fd6f8f326bf13655e58211ff62dae
york yao <yaoao12306@outlook.com>
2017-07-16 08:27:42 +0800

    new release

--- dist/config.js
+++ dist/config.js
@@ -1,6 +1,63 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const configurationFilePath = process.argv[2] || "../log-tool.config.js";
-// tslint:disable-next-line:no-var-requires
-_a = require(configurationFilePath), exports.elastic = _a.elastic, exports.gui = _a.gui, exports.inflow = _a.inflow, exports.outflow = _a.outflow, exports.watcher = _a.watcher, exports.protobuf = _a.protobuf, exports.folderSizeWatcher = _a.folderSizeWatcher, exports.countLogs = _a.countLogs, exports.os = _a.os, exports.sqlite = _a.sqlite;
-var _a;
+const libs = require("./libs");
+const defaultConfig = {
+    inflow: {
+        enabled: true,
+        port: 8001,
+        host: "localhost",
+        httpFallbackPath: "/logs",
+    },
+    outflow: {
+        enabled: false,
+        url: "ws://localhost:8001",
+    },
+    watcher: {
+        enabled: true,
+        paths: [],
+        filePositionsDataPath: "./log-tool.watcher.data",
+        parseLine: (line, moment, filepath) => {
+            return {
+                skip: false,
+                time: moment().format("YYYY-MM-DD HH:mm:ss"),
+                content: line,
+            };
+        },
+    },
+    gui: {
+        enabled: true,
+        port: 8000,
+        host: "localhost",
+    },
+    elastic: {
+        enabled: true,
+        // `tool` is the index name, `logs` is the type name, they are all needed.
+        url: "http://localhost:9200/tool/logs",
+    },
+    protobuf: {
+        enabled: true,
+    },
+    folderSizeWatcher: {
+        enabled: true,
+        // tslint:disable-next-line:no-object-literal-type-assertion
+        folders: {},
+    },
+    countLogs: {
+        enabled: true,
+    },
+    os: {
+        enabled: true,
+    },
+    sqlite: {
+        filePath: "./data.db",
+        samples: true,
+    },
+};
+try {
+    const configurationFilePath = process.argv[2] || "../log-tool.config.js";
+    // tslint:disable-next-line:no-var-requires
+    require(configurationFilePath)(defaultConfig);
+}
+catch (error) {
+    libs.print(error);
+}
+module.exports = defaultConfig;
--- static/index.config.js
+++ static/index.config.js
@@ -1,72 +1,27 @@
-/**
- * fields:
- *   enabled: boolean
- *   name: string
- *   description: string
- *   unit?: string;
- *   unitScale?: number, eg: { unit: "KB", unitScale: 1024 }, 10240 B -> 10 KB
- *   compute?: (sample: { [name: string]: number }) => number, see the example below
- */
-var chartConfigs = [
-  {
-    enabled: true,
-    name: 'logCount',
-    description: '日志数'
-  },
-  {
+function loadConfig (config) { // eslint-disable-line no-unused-vars
+  config.chart.push({
     enabled: true,
     name: 'mysqlSize',
     description: 'mysql数据',
     unit: 'kB',
     unitScale: 1024
-  },
-  {
-    enabled: true,
-    name: 'cpu',
-    description: 'CPU',
-    unit: '%'
-  },
-  {
-    enabled: true,
-    name: 'memory',
-    description: '内存',
-    unit: '%'
-  },
-  {
+  })
+  config.chart.push({
     enabled: false,
     name: 'httpRequestCount',
     description: 'HTTP请求数'
-  },
-  {
+  })
+  config.chart.push({
     enabled: false,
     name: 'httpResponseTime',
     description: 'HTTP响应总耗时',
     unit: 'ms'
-  },
-  {
+  })
+  config.chart.push({
     enabled: false,
     name: 'httpAverageResponsesTime',
     description: 'HTTP响应平均耗时',
     unit: 'ms',
     compute: sample => sample.httpRequestCount === 0 ? 0 : Math.round(sample.httpResponseTime / sample.httpRequestCount)
-  }
-]
-
-/* eslint-disable no-unused-vars */
-var protobufConfig = {
-  enabled: true
-}
-/* eslint-enable no-unused-vars */
-
-chartConfigs = chartConfigs.filter(config => config.enabled)
-for (const config of chartConfigs) {
-  if (!('unit' in config)) {
-    config.unit = undefined
-  }
-  if (!('unitScale' in config)) {
-    config.unitScale = undefined
-  }
-  if (!('compute' in config)) {
-    config.compute = undefined
-  }
+  })
 }
